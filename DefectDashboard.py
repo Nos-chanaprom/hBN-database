77f41d38411956fd555d029f90f733a44d94bc32
Nos-Chanaprom-Cholsuk <chanapromc@gmail.com>
2025-06-19 16:41:10 +0200

    Update DefectDashboard.py

--- DefectDashboard.py
+++ DefectDashboard.py
@@ -207,7 +207,7 @@ def plot_diagram_plotly(data, title):
         xaxis_range=[0, 6],
         yaxis_range=[min_energy - 0.5, max_energy + 0.5],  # Padding for aesthetics
         width=600,
-        height=400,
+        height=800,
         legend=dict(
             x=0.02,
             y=0.98,

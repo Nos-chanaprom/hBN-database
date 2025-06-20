8f9b63b55e23a81808274c70fb868c565451d999
Nos-Chanaprom-Cholsuk <chanapromc@gmail.com>
2025-06-20 14:17:13 +0200

    Update DefectDashboard.py

--- DefectDashboard.py
+++ DefectDashboard.py
@@ -219,7 +219,9 @@ def plot_diagram_plotly(data, title,base_font: int = 12):
     )
     fig.update_layout(
         #title=title,   # title of the plot
-        template="plotly_white",
+        template="plotly_white",       # still grab all the white-template defaults…
+        paper_bgcolor="white",         # …and force the outside margin to white
+        plot_bgcolor="white",          # …and force the inside plotting area to white
         font=dict(size=18, color="Black"),
         showlegend=True,
         xaxis_range=[0, 6],

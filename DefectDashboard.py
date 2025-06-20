052b43861dfa2cec6667ed0a228ff64613f1d2d9
Nos-Chanaprom-Cholsuk <chanapromc@gmail.com>
2025-06-20 13:23:56 +0200

    Update DefectDashboard.py

--- DefectDashboard.py
+++ DefectDashboard.py
@@ -1262,11 +1262,11 @@ for tabs in tab_selection:
                         st.header("Defect Formation Energy of "+"${}$".format(latexdefect))
                         tab1, tab2 = st.tabs(["N-rich","N-poor"])
                         with tab1:                
-                            st.components.v1.html(fig_rich.to_html(include_mathjax='cdn',full_html=False,div_id='formation-rich'),width=550, height=600)
-                            #st.plotly_chart(fig_rich, use_container_width=True)   #  ← change
+                            #st.components.v1.html(fig_rich.to_html(include_mathjax='cdn',full_html=False,div_id='formation-rich'),width=550, height=600)
+                            st.plotly_chart(fig_rich, use_container_width=True,theme="streamlit")   #  ← change
                         with tab2: 
-                            st.components.v1.html(fig_poor.to_html(full_html=False,include_plotlyjs=False,div_id='formation-poor'),width=550, height=600)
-                            #st.plotly_chart(fig_poor, use_container_width=True)   #  ← change
+                            #st.components.v1.html(fig_poor.to_html(include_mathjax='cdn',full_html=False,div_id='formation-poor'),width=550, height=600)
+                            st.plotly_chart(fig_poor, use_container_width=True, theme='streamlit')   #  ← change
 
                 ###### for PL spectrum
                 # Path to the PL file

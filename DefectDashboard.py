8c711073bd7a5ef05c28845b3b51a14c6cb6d4b7
Nos-Chanaprom-Cholsuk <chanapromc@gmail.com>
2025-06-21 14:56:24 +0200

    Update DefectDashboard.py

--- DefectDashboard.py
+++ DefectDashboard.py
@@ -1089,7 +1089,7 @@ for tabs in tab_selection:
 
             with col4:
                 with st.container(border=True):
-                    st.header("Luminescence spectrum of "+"${}$".format(latexdefect))
+                    st.header("Luminescence spectrum of "+"${}$".format(latexdefect)+str_charge)
                     tab1, tab2 = st.tabs(["Photoluminescence","Absorption"])
                     with tab1:
                                 # Check if the file exists

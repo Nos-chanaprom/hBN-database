3b35889e7f83623ca2ad814f4bbf63af649b6c2d
Nos-Chanaprom-Cholsuk <chanapromc@gmail.com>
2025-07-23 09:47:20 +0200

    Update DefectDashboard.py

--- DefectDashboard.py
+++ DefectDashboard.py
@@ -14,7 +14,7 @@ import os
 import warnings
 import time
 import plotly.colors as pc
-
+import matplotlib.pyplot as plt  # For plotting Raman
 import sqlite3  # Added for DB support
 
 @st.cache_data
